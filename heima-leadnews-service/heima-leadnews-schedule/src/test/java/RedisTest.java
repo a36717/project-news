beb1015fb89e7e006a8d51fac472deba7aeb4374
Seele <367177408@qq.com>
2024-04-23 15:50:11 +0800

    kafka实现发布端与app端消息同步

--- heima-leadnews-service/heima-leadnews-schedule/src/test/java/RedisTest.java
+++ heima-leadnews-service/heima-leadnews-schedule/src/test/java/RedisTest.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 
@@ -43,4 +46,6 @@ public class RedisTest {
 
 
     }
+
+
 }
